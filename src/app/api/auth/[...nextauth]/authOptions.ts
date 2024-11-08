c56f15b6dbc9859cd7c6dc4744006652c9ac886c
Idhibhat Pankam <marginpankam@gmail.com>
2024-11-08 14:56:51 +0700

    setup nextauth

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,44 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials) return null;
+
+        // you can do the login logic here
+
+        // const user = await userLogIn(credentials.email, credentials.password);
+        // if (user instanceof Error) {
+        //   throw new Error("Failed to login");
+        // }
+
+        // if (user) {
+        //   return user;
+        // }
+
+        return null;
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+};
