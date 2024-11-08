06cb9f9bc32160c45bbadca11de826e9540d26fc
Idhibhat Pankam <marginpankam@gmail.com>
2024-11-08 15:05:56 +0700

    navbar

--- src/components/NavBar/NavBarItem.tsx
+++ src/components/NavBar/NavBarItem.tsx
@@ -1,3 +1,17 @@
-export const NavBarItem = () => {
-  return <div>nav bar item</div>;
+import Link from "next/link";
+import { FC } from "react";
+
+interface NavBarItemProps {
+  name: string;
+  path: string;
+}
+
+export const NavBarItem: FC<NavBarItemProps> = ({ name, path }) => {
+  return (
+    <Link href={path} className="h-full">
+      <div className="text-lightgray hover:bg-grayhl flex h-full items-center px-3 transition duration-150 ease-in-out">
+        {name}
+      </div>
+    </Link>
+  );
 };
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,3 +1,25 @@
-export const NavBar = () => {
-  return <div>nav bar</div>;
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { getServerSession } from "next-auth";
+import { Logo } from "../Logo";
+import { NavBarItem } from "./NavBarItem";
+
+export const NavBar = async () => {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <div className="sticky top-0 z-50 flex h-16 w-full justify-around border-b">
+      <div className="flex w-[20%] items-center">
+        {session ? (
+          <NavBarItem name="Logout" path="/api/auth/signout" />
+        ) : (
+          <NavBarItem name="Login" path="/api/auth/signin" />
+        )}
+        <NavBarItem name="My Bookings" path="/mybooking" />
+      </div>
+      <div className="flex w-[40%] items-center justify-between md:w-[20%]">
+        <NavBarItem name="Booking" path="/booking" />
+        <Logo />
+      </div>
+    </div>
+  );
 };
