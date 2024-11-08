481f1550ed65fc333987ea7684286a7f318a8e3b
Idhibhat Pankam <marginpankam@gmail.com>
2024-11-08 21:53:55 +0700

    see profile

--- src/app/dentists/DentistsGrid.tsx
+++ src/app/dentists/DentistsGrid.tsx
@@ -11,7 +11,7 @@ export const DentistsGrid = async () => {
   return (
     <div className="grid grid-cols-3 gap-4">
       {dentists.map((dentist) => (
-        <div className="overflow-hidden">
+        <div className="flex h-[35vh] flex-col items-center">
           <Image
             className="h-[150px] w-[150px] rounded-full object-cover"
             src={dentist.picture}
@@ -20,7 +20,25 @@ export const DentistsGrid = async () => {
             height={150}
             unoptimized
           />
-          {dentist.name}
+          <p className="mt-4 text-2xl font-bold">{dentist.name}</p>
+          <p className="mt-2 text-lg">{dentist.expertist}</p>
+          <div className="hover:bg-pink mt-4 flex w-full justify-center rounded-md bg-gray-200 py-2 transition duration-150 ease-in-out hover:text-white">
+            See Profile
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const DentistsGridSuspense = async () => {
+  const dentists: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {dentists.map((_) => (
+        <div className="flex h-[35vh] flex-col items-center">
+          <div className="h-[150px] w-[150px] rounded-full bg-gray-300"></div>
         </div>
       ))}
     </div>
--- src/app/dentists/page.tsx
+++ src/app/dentists/page.tsx
@@ -1,15 +1,15 @@
 // "use client";
 
 import { Suspense } from "react";
-import { DentistsGrid } from "./DentistsGrid";
+import { DentistsGrid, DentistsGridSuspense } from "./DentistsGrid";
 
 export default async function DentistsPage() {
   // const { dentists } = useFindAllDentist();
 
   return (
     <main className="mt-[10vh] px-[10vw]">
-      <h1 className="text-5xl font-bold">Dental Team and Staffs</h1>
-      <Suspense fallback={<p>a</p>}>
+      <h1 className="mb-12 text-5xl font-bold">Dental Team and Staffs</h1>
+      <Suspense fallback={<DentistsGridSuspense />}>
         <DentistsGrid />
       </Suspense>
     </main>
