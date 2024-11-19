3be476673d48d0fdc3cd4dd1a4703cf8e0869919
Idhibhat Pankam <marginpankam@gmail.com>
2024-11-19 20:52:55 +0700

    fix booking list

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -1,27 +1,36 @@
 "use client";
-import { removeBooking } from "@/redux/features/bookSlice";
-import { AppDispatch, useAppSelector } from "@/redux/store";
+import { removeBooking } from "@/store/bookSlice";
+import { AppDispatch, useAppSelector } from "@/store/store";
 import { useDispatch } from "react-redux";
 
 export default function BookingList() {
-  const bookingItems = useAppSelector((state) => state.bookSlice.bookItems);
+  const bookingItems = useAppSelector((state) => state.bookReducer.bookItems);
   const dispatch = useDispatch<AppDispatch>();
 
   return (
-    <>  <div className="text-xl flex justify-center items-center">การจองวัคซีน</div>
+    <>
+      {" "}
+      <div className="flex items-center justify-center text-xl">
+        การจองวัคซีน
+      </div>
       {bookingItems.length === 0 ? (
-        <div className="text-xl flex justify-center items-center">No Vaccine Booking</div>
+        <div className="flex items-center justify-center text-xl">
+          No Vaccine Booking
+        </div>
       ) : (
         bookingItems.map((bookItem) => (
-          <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2" key={bookItem._id}>
-            <div className="text-xl text-black">
-        
-            </div>
+          <div
+            className="mx-5 my-2 rounded bg-slate-200 px-5 py-2"
+            key={bookItem._id}
+          >
+            <div className="text-xl text-black"></div>
             <div className="text-xl text-black">รหัสประจำตัวประชาชน: </div>
             <div className="text-xl text-black">โรงพยาบาล:</div>
-            <div className="text-xl text-black">วันที่ต้องการรับวัคซีน: {bookItem.bookDate}</div>
+            <div className="text-xl text-black">
+              วันที่ต้องการรับวัคซีน: {bookItem.bookDate}
+            </div>
             <button
-              className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-1 text-white shadow-sm"
+              className="block rounded-md bg-sky-600 px-3 py-1 text-white shadow-sm hover:bg-indigo-600"
               onClick={() => dispatch(removeBooking(bookItem._id))}
             >
               Remove Booking
