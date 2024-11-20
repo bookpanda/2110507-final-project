44526d682e4809354b5004a226db260a27b1d597
Idhibhat Pankam <marginpankam@gmail.com>
2024-11-20 22:13:03 +0700

    youre not logged in

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,19 +1,38 @@
 "use client";
 
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Button } from "./Button";
 
 interface BannerProps {}
 
 const Banner: FC<BannerProps> = () => {
+  const { data: session, status } = useSession();
+  const [open, setOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="relative h-[80vh] w-full">
       <div className="z-10 flex flex-row items-center justify-center gap-x-40 p-20 text-white md:items-start">
         <div className="w-1/3">
-          <h1 className="text-orange text-5xl italic">Special Dental Clinic</h1>
-          <p className="text-pink mb-4 mt-2 text-3xl italic">
+          <h1 className="text-5xl italic text-orange">Special Dental Clinic</h1>
+          <p className="mb-4 mt-2 text-3xl italic text-pink">
             Faculty of Dentistry, Chaluฯ
           </p>
           <p className="mt-2 text-2xl italic text-black">
@@ -25,9 +44,34 @@ const Banner: FC<BannerProps> = () => {
             <Link href="/dentists">
               <Button variant="secondary" text="Our dentists"></Button>
             </Link>
-            <Link href="/booking">
-              <Button variant="primary" text="Book an appointment"></Button>
-            </Link>
+            {session ? (
+              <Link href="/booking">
+                <Button variant="primary" text="Book an appointment"></Button>
+              </Link>
+            ) : (
+              <>
+                <Button
+                  variant="primary"
+                  text="Book an appointment"
+                  onClick={handleButtonClick}
+                ></Button>
+                <Dialog open={open} onClose={handleClose}>
+                  <DialogTitle>{"You Are Not Logged In"}</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      Please log in to create a booking.
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button
+                      variant="primary"
+                      text="Close"
+                      onClick={handleClose}
+                    ></Button>
+                  </DialogActions>
+                </Dialog>
+              </>
+            )}
           </div>
         </div>
         <div className="w-1/3">
